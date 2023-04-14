7a83099a5fbccb74a31b361bb44938603f5eb1fe
谢忠泽 <xiezhongze@feewee.cn>
2023-04-14 16:27:24 +0800

    fix:测试构建

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index3</h1>
+      <h1 className={styles.title}>Page index4</h1>
     </div>
   );
 }
